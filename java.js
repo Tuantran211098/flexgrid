51c388bc393a42bdb31cc026ae5783af6fea45dc
tuan <tuantran21101998@gmail.com>
2020-09-22 15:42:38 +0700

    first commit

---
+++ java.js
@@ -0,0 +1,34 @@
+//function showImage(src){
+//    document.getElementById("my_image").src = src
+//}
+//var demos=true ;
+//function test(){
+//    document.getElementById("demo").innerHTML = demos
+//    console.log(test)
+//}
+//function myFunction(){
+//    var age,turia;
+//    age = Number(document.getElementById('age').value);
+//    if(isNaN(age)){
+//        turia = 'input not number'
+//    }
+//    else{
+//         turia = (age > 18) ? 'wrong':'right'
+//    }
+//    document.getElementById('demos').innerHTML = turia
+//}
+//function myFunctions(){
+//    var str = document.getElementById('demo').innerHTML;
+//    document.getElementById('demo').innerHTML= str.toUpperCase();
+//}
+//function clickFunction(src){
+//    document.getElementById('imgage').src= src
+//}
+//var fruits = ["banana",'orange',"apple"]
+//var flen = fruits.length
+//var text= "<ul>";
+//for (var i =0;i<flen;i++){
+//    text += "<li>"+ fruits[i]+"</li>"
+//}
+//text += "</ul>";
+//document.getElementById('demo').innerHTML= text
